e0c39a28c3f63ea89c57d4eda4ea02b92e19a7b1
chaptsand <chaptsand@gmail.com>
2024-01-29 21:12:17 +0800

    build
    Change-Id: Idcdef2bc95290e0ae0be26425fcb04792f2d159d

--- extmod.mk
+++
@@ -1,6 +0,0 @@
-core-y		+= drivers/samsung/debug/boot_stat/
-core-y		+= drivers/samsung/debug/log_buf/
-core-y		+= drivers/samsung/debug/arm64/ap_context/
-core-y		+= drivers/base/regmap/
-core-y		+= drivers/spmi/
-core-y		+= drivers/interconnect/qcom/
