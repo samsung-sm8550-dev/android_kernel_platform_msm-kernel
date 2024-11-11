947c698eee67ec940122ddff7eb57b2c264c899c
chaptsand <chaptsand@gmail.com>
2024-11-11 14:47:16 +0800

    drivers/char: Import SAMSUNG changes

--- drivers/char/adsprpc_shared.h
+++ drivers/char/adsprpc_shared.h
@@ -698,6 +698,9 @@ enum fastrpc_msg_type {
 // Must be a power of two.
 #define DSPSIGNAL_GROUP_SIZE 256
 
+#define SS_MEM_PROFILE
+#define SS_MEM_DEBUG
+#define SS_FASTRPC_SYNC
 
 struct secure_vm {
 	int *vmid;
@@ -727,6 +730,11 @@ struct fastrpc_buf {
 	bool in_use;	/* Used only for persistent header buffers */
 	struct timespec64 buf_start_time;
 	struct timespec64 buf_end_time;
+
+#if defined(SS_MEM_DEBUG)
+	uint32_t pid;           /* alloc real pid */
+	char comm[TASK_COMM_LEN];
+#endif
 };
 
 struct fastrpc_ctx_lst;
@@ -981,9 +989,15 @@ struct fastrpc_mmap {
 	bool in_use;				/* Indicates if persistent map is in use*/
 	struct timespec64 map_start_time;
 	struct timespec64 map_end_time;
-	bool is_filemap;			/*flag to indicate map used in process init*/
+	bool is_filemap;			/* flag to indicate map used in process init */
+	bool is_dumped;				/* flag to indicate map is dumped during SSR */
 	char *servloc_name;			/* Indicate which daemon mapped this */
 	unsigned int ctx_refs; /* Indicates reference count for context map */
+
+#if defined(SS_MEM_DEBUG)
+	uint32_t pid;           /* alloc real pid */
+	char comm[TASK_COMM_LEN];
+#endif
 };
 
 enum fastrpc_perfkeys {
@@ -1119,6 +1133,17 @@ struct fastrpc_file {
 	bool exit_notif;
 	/* Flag to indicate async thread exit requested*/
 	bool exit_async;
+
+#if defined(SS_MEM_PROFILE)
+	unsigned int len_curr_usage;
+	unsigned int len_peak_usage;
+
+	unsigned int page_curr_usage;
+	unsigned int page_peak_usage;
+
+	struct mem_profile_private *mem_profile;
+#endif
+
 };
 
 union fastrpc_ioctl_param {
