9174cdc7d905a12ca3d9e4fa809170bb0ce6cdb7
chaptsand <chaptsand@gmail.com>
2024-11-11 14:47:20 +0800

    drivers/input/misc: Import SAMSUNG changes

--- drivers/input/misc/pm8941-pwrkey.c
+++ drivers/input/misc/pm8941-pwrkey.c
@@ -20,6 +20,13 @@
 #include <linux/reboot.h>
 #include <linux/regmap.h>
 #include <linux/suspend.h>
+#if IS_ENABLED(CONFIG_SEC_PM)
+#include <linux/mfd/sec_ap_pmic.h>
+
+#define PON_PBS_KPDPWR_S2_CNTL2			0x43
+#define PON_PBS_KPDPWR_RESIN_S2_CNTL2	0x4B
+#define PON_PBS_S2_CNTL_EN				BIT(7)
+#endif
 
 #define PON_REV2			0x01
 
@@ -83,7 +90,76 @@ struct pm8941_pwrkey {
 	bool last_status;
 	bool pull_up;
 	const struct pm8941_data *data;
+
+#if IS_ENABLED(CONFIG_SEC_PM)
+	int wake_enabled;
+#endif
 };
+
+#if IS_ENABLED(CONFIG_SEC_PM)
+static struct _sec_pon {
+	struct pm8941_pwrkey *key_dev[SEC_PON_KEY_CNT];
+	struct pm8941_pwrkey *reset_dev;
+} sec_pon;
+
+int sec_get_s2_reset(enum sec_pon_type type)
+{
+	int rc;
+	unsigned int en;
+	struct pm8941_pwrkey *pwrkey = sec_pon.reset_dev;
+ 	unsigned int offset;
+
+	if (type != SEC_PON_KPDPWR && type != SEC_PON_KPDPWR_RESIN)
+		return -EINVAL;
+	if (!pwrkey)
+		return -ENXIO;
+
+	offset = (type == SEC_PON_KPDPWR) ?
+				PON_PBS_KPDPWR_S2_CNTL2 : PON_PBS_KPDPWR_RESIN_S2_CNTL2;
+
+	rc = regmap_read(pwrkey->regmap,
+			    pwrkey->pon_pbs_baseaddr + offset, &en);
+	if (rc) {
+		pr_err("Unable to get S2 reset on/off for type %d\n", type);
+		return rc;
+	}
+
+	return (en & PON_PBS_S2_CNTL_EN) ? true : false;
+}
+EXPORT_SYMBOL_GPL(sec_get_s2_reset);
+
+// Key wakeup enable for (SEC_PON_KPDPWR, SEC_PON_RESIN)
+int sec_set_pm_key_wk_init(enum sec_pon_type type, int en)
+{
+	struct pm8941_pwrkey *pwrkey;
+
+	if (type != SEC_PON_KPDPWR && type != SEC_PON_RESIN)
+		return -EINVAL;
+
+	pwrkey = sec_pon.key_dev[type];
+	if (!pwrkey)
+		return -ENXIO;
+	
+	if (pwrkey->wake_enabled != en) {
+		pwrkey->wake_enabled = en;
+		pr_info("%s: wk_int (type:%d, en:%d)\n", __func__, type, en);
+	}
+	else {
+		pr_info("%s: already wk_int (type:%d, en:%d)\n", __func__, type, en);
+	}
+	return 0;
+}
+EXPORT_SYMBOL_GPL(sec_set_pm_key_wk_init);
+
+int sec_get_pm_key_wk_init(enum sec_pon_type type)
+{
+	if (type != SEC_PON_KPDPWR && type != SEC_PON_RESIN)
+		return -EINVAL;
+
+	return sec_pon.key_dev[type]->wake_enabled;
+}
+EXPORT_SYMBOL_GPL(sec_get_pm_key_wk_init);
+#endif
 
 static int pm8941_reboot_notify(struct notifier_block *nb,
 				unsigned long code, void *unused)
@@ -298,7 +374,15 @@ static int pm8941_pwrkey_suspend(struct device *dev)
 		return pm8941_pwrkey_freeze(dev);
 
 	if (device_may_wakeup(dev))
+	{
+#if IS_ENABLED(CONFIG_SEC_PM)
+		if (!pwrkey->wake_enabled) {
+			pr_info("%s: enable_irq_wake skip by sec_ap_pmic\n", pwrkey->input->name);
+			return 0;
+		}
+#endif
 		enable_irq_wake(pwrkey->irq);
+	}
 
 	return 0;
 }
@@ -311,7 +395,15 @@ static int pm8941_pwrkey_resume(struct device *dev)
 		return pm8941_pwrkey_restore(dev);
 
 	if (device_may_wakeup(dev))
+	{
+#if IS_ENABLED(CONFIG_SEC_PM)
+		if (!pwrkey->wake_enabled) {
+			pr_info("%s: disable_irq_wake skip by sec_ap_pmic\n", pwrkey->input->name);
+			return 0;
+		}
+#endif
 		disable_irq_wake(pwrkey->irq);
+	}
 
 	return 0;
 }
@@ -466,6 +558,22 @@ static int pm8941_pwrkey_probe(struct platform_device *pdev)
 	platform_set_drvdata(pdev, pwrkey);
 	device_init_wakeup(&pdev->dev, 1);
 
+#if IS_ENABLED(CONFIG_SEC_PM)
+	if (pwrkey->data->has_pon_pbs) {
+		if (!strncmp(pwrkey->data->name, "pmic_pwrkey", 11)) {
+			pr_info("%s: pon_kpdpwr", __func__);
+			sec_pon.key_dev[SEC_PON_KPDPWR] = pwrkey;
+			sec_pon.reset_dev = pwrkey;
+			pm8941_pwrkey_irq(pwrkey->irq, pwrkey);
+		} else if (!strncmp(pwrkey->data->name, "pmic_resin", 10)) {
+			pr_info("%s: pon_resin", __func__);
+			sec_pon.key_dev[SEC_PON_RESIN] = pwrkey;
+			pm8941_pwrkey_irq(pwrkey->irq, pwrkey);
+		}
+	}
+	pwrkey->wake_enabled = true;
+#endif
+
 	return 0;
 }
 
@@ -521,11 +629,22 @@ static const struct pm8941_data pon_gen3_resin_data = {
 	.has_pon_pbs = true,
 };
 
+static const struct pm8941_data pon_gen3_resin_data_flex = {
+	.status_bit = PON_GEN3_RESIN_N_SET,
+	.name = "pmic_resin_flex",
+	.phys = "pmic_resin/input0",
+	.supports_ps_hold_poff_config = false,
+	.supports_debounce_config = false,
+	.needs_sw_debounce = true,
+	.has_pon_pbs = true,
+};
+
 static const struct of_device_id pm8941_pwr_key_id_table[] = {
 	{ .compatible = "qcom,pm8941-pwrkey", .data = &pwrkey_data },
 	{ .compatible = "qcom,pm8941-resin", .data = &resin_data },
 	{ .compatible = "qcom,pmk8350-pwrkey", .data = &pon_gen3_pwrkey_data },
 	{ .compatible = "qcom,pmk8350-resin", .data = &pon_gen3_resin_data },
+	{ .compatible = "qcom,pmk8350-resin_flex", .data = &pon_gen3_resin_data_flex },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, pm8941_pwr_key_id_table);
